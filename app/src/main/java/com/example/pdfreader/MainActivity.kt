091252c8fcf3bd0ec6aa970650e676a1976a957c
hazem amaar <62269304+hazemamaar@users.noreply.github.com>
2023-02-13 22:57:04 +0200

    last update

--- app/src/main/java/com/example/pdfreader/MainActivity.kt
+++ app/src/main/java/com/example/pdfreader/MainActivity.kt
@@ -14,11 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var textToSpeech: TextToSpeech? = null
     var extractedText = ""
-    private val READ_REQUEST_CODE = 42
-    private val PRIMARY = "primary"
-    private val LOCAL_STORAGE = "/storage/self/primary/"
-    private val EXT_STORAGE = "/storage/7764-A034/"
-    private val COLON = ":"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,78 +22,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.outputText.movementMethod = ScrollingMovementMethod()
-        extractData()
+
         textToSpeech = TextToSpeech(
             applicationContext
         ) {
-            if(it != TextToSpeech.ERROR)
-               textToSpeech?.language = Locale.US
+            if (it != TextToSpeech.ERROR)
+                textToSpeech?.language = Locale.US
         }
-      binding.fab.setOnClickListener {
-          textToSpeech!!.speak(extractedText, TextToSpeech.QUEUE_FLUSH, null,null )
-      }
+        textToSpeech()
+        extractData()
 
-//        binding.fab.setOnClickListener{
-//                intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-//                intent.type = "*/*"
-//            registerResult.launch(intent)
-//            }
 
     }
-//    private val registerResult =
-//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-//            if (it.data != null) {
-//                if (it.resultCode == Activity.RESULT_OK) {
-//                    val uri = it.data?.data.toString().toUri()
-//                    Log.e("hhhh ", ": ${uri.path}", )
-//                    readPdfFile("assets/haz.pdf".toUri())
-//                }
-//            }
-//        }
-//    fun readPdfFile(uri: Uri) {
-//        val fullPath: String
-//        //convert from uri to full path
-//        fullPath = if (uri.path!!.contains(PRIMARY)) {
-//            LOCAL_STORAGE + uri.path?.split(COLON)?.get(1)
-//        } else {
-//            EXT_STORAGE + uri.path?.split(COLON)?.get(1)
-//        }
-//        Log.v("URI", uri.path + " " + fullPath)
-//        val stringParser: String
-//        try {
-//            val pdfReader = PdfReader(fullPath)
-//            stringParser = PdfTextExtractor.getTextFromPage(pdfReader, 1).trim()
-//            pdfReader.close()
-//            Log.e("nnnn", "readPdfFile: $stringParser" )
-//            binding.outputText.text = stringParser
-//            textToSpeech!!.speak(stringParser, TextToSpeech.QUEUE_FLUSH, null, null)
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
-//    }
+
+    private fun textToSpeech() {
+        binding.fab.setOnClickListener {
+            textToSpeech!!.speak(extractedText, TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+    }
 
     private fun extractData() {
-        // on below line we are running a try and catch block
-        // to handle extract data operation.
         try {
-            // on below line we are creating a
-            // variable for storing our extracted text
-
-
-            // on below line we are creating a
-            // variable for our pdf extracter.
-            val pdfReader: PdfReader = PdfReader("res/raw/haz.pdf")
-
-            // on below line we are creating
-            // a variable for pages of our pdf.
+            val pdfReader = PdfReader("res/raw/haz.pdf")
             val n = pdfReader.numberOfPages
-
-            // on below line we are running a for loop.
             for (i in 0 until n) {
-
-                // on below line we are appending
-                // our data to extracted
-                // text from our pdf file using pdf reader.
                 extractedText =
                     """
                  $extractedText${
@@ -105,22 +53,10 @@ class MainActivity : AppCompatActivity() {
                     }
                   
                  """.trimIndent()
-                // to extract the PDF content from the different pages
             }
-
-            // on below line we are setting
-            // extracted text to our text view.
             binding.outputText.text = extractedText
-
-
-            // on below line we are
-            // closing our pdf reader.
             pdfReader.close()
-
-        }
-        // on below line we are handling our
-        // exception using catch block
-        catch (e: Exception) {
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
